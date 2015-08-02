2373d390db43cc8f3869aacde967d15398977be8
Denis Glazkov <denis@begom.com>
2015-08-02 09:40:33 +0300

    init repository

---
+++ basenetworkd.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+/**
+ * base.network NodeJS-server 0.1 (c) 2015 Denis Glazkov | https://github.com/basenetwork/
+ *
+ * http://base.network/
+ *
+ */
+
+//------- start node-server -----------
+require("./lib/").startServer(process.argv);
