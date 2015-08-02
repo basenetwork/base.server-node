718490deba58553c70a4683c51be77a25a93f92a
Denis Glazkov <denis@begom.com>
2015-08-02 10:02:22 +0300

    changed readme

--- README.md
+++ README.md
@@ -5,8 +5,6 @@ http://base.network
 Install server node
 -------------------
 ### Linux
-`git clone https://github.com/basenetwork/base.server-node && cd base.server-node`
+`git clone https://github.com/basenetwork/base.server-node`
 
-`npm install sqlite3 --build-from-source`
-
-`./basenetworkd.js  >/var/log/base.network.log &`
+`base.server-node/basenetworkd.js  >/var/log/base.network.log &`
--- basenetworkd.js
+++ basenetworkd.js
@@ -7,5 +7,18 @@
  *
  */
 
+try {
+    require("sqlite3");
+} catch(e) {
+    console.log("\n\n" +
+    "\n========= ERROR: require('sqlite3') ===============" +
+    "\n(error details: " + e + ")" +
+    "\n" +
+    "\nTry to install module from sources:" +
+    "\n" +
+    "\n       npm install sqlite3 --build-from-source" +
+    "\n");
+}
+
 //------- start node-server -----------
 require("./lib/").startServer(process.argv);
